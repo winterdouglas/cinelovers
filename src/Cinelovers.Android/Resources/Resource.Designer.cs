cd8b1388b78ff0c36677e83d6ad186c260163ae3
Douglas Winter <a.winterdoug@gmail.com>
2017-11-19 23:02:16 -0200

    Added ReactiveUI

--- src/Cinelovers.Android/Resources/Resource.Designer.cs
+++ src/Cinelovers.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace Cinelovers.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Splat.Resource.String.library_name = global::Cinelovers.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Cinelovers.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3002,6 +3003,9 @@ namespace Cinelovers.Droid
 			
 			// aapt resource value: 0x7f080025
 			public const int character_counter_pattern = 2131230757;
+			
+			// aapt resource value: 0x7f080026
+			public const int library_name = 2131230758;
 			
 			// aapt resource value: 0x7f080000
 			public const int mr_button_content_description = 2131230720;
