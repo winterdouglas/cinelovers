87d776cba96b5384e1cde2549746aa9df8d9fb30
Douglas Winter <a.winterdoug@gmail.com>
2017-11-20 01:08:24 -0200

    Changed test projects, Aligned Nunit version; Removed unused packages for now.

--- src/Cinelovers.Infrastructure.UnitTests/Properties/AssemblyInfo.cs
+++ src/Cinelovers.Infrastructure.UnitTests/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cinelovers.Infrastructure.UnitTests")]
@@ -11,19 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cinelovers.Infrastructure.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd3b26d1-3c01-4d07-80a8-477ff34fc04f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- src/Cinelovers.ViewModels.UnitTests/Movies/UpcomingMoviesViewModelTests.cs
+++ src/Cinelovers.ViewModels.UnitTests/Movies/UpcomingMoviesViewModelTests.cs
@@ -5,9 +5,5 @@ namespace Cinelovers.ViewModels.UnitTests.Movies
     [TestFixture]
     public class UpcomingMoviesViewModelTests
     {
-        [Test]
-        public void Test()
-        {
-        }
     }
 }
